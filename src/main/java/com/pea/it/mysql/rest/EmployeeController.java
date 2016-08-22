f17e809554dbccc7a787dbc9dcb8fe685a8149c9
selina <2356514329@qq.com>
2016-08-23 06:42:07 +0800

    update

--- src/main/java/com/pea/it/mysql/rest/EmployeeController.java
+++ src/main/java/com/pea/it/mysql/rest/EmployeeController.java
@@ -2,6 +2,7 @@ package com.pea.it.mysql.rest;
 
 import com.pea.it.mysql.domain.Employee;
 import com.pea.it.mysql.service.EmployeeService;
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Created by selina on 2016/8/17.
@@ -24,6 +27,17 @@ public class EmployeeController {
 
     @RequestMapping(value = "/{empNo}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Employee getById(@PathVariable("empNo") Long empNo) {
+        long l = RandomUtils.nextLong(10000, 310000);
         return employeeService.findById(empNo);
+    }
+
+    @RequestMapping(value = "/random", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Employee getRandomEmp() {
+        long empNo = RandomUtils.nextLong(10000, 310000);
+        return employeeService.findById(empNo);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RandomUtils.nextLong(10000, 310000));
     }
 }
